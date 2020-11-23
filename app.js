a798506bb5f3256b91a06ee6ce9f2b04ca04c253
azlan <mhdazlanabaziz@gmai.com>
2020-11-23 15:34:52 +0800

    implemented static page; ;

--- app.js
+++ app.js
@@ -1,171 +1,31 @@
 const express = require('express');
-const fs = require('fs');
+
 const morgan = require('morgan');
 
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 const app = express();
 
+//add middleware
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
-//adding middleware
+//adding custom middleware
 app.use((req, res, next) => {
-  console.log('Hello from the middleware ');
+  console.log('Hello from the middleware 🧻 ');
   next();
 });
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      // the object   the data
-      pelancongan: tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  console.log(req.params);
-  console.log(req.requestTime);
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: { tour },
-    // tour is declared above
-  });
-};
-
-const createTour = (req, res) => {
-  console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-  // status 200 is ok
-  // status 201 is created
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: { tour: '<Updated tour here...>' },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  //204 means no content
-  res.status(204).json({
-    status: 'success',
-    data: { tour: '<Updated tour here...>' },
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// //apply update verb
-// app.patch('/api/v1/tours/:id', updateTour);
-// //delete request
-// app.delete('/api/v1/tours/:id', deleteTour);
-
-// ROUTES
-const tourRouter = express.Router();
-app.use('/api/v1/tours', tourRouter);
-
-tourRouter.route('/').get(getAllTours).post(createTour);
 
 // second middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString;
   next();
 });
+// ROUTES
 
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+app.use('/api/v1/tours', tourRouter);
+// mounting a router on a new route
+app.use('/api/v1/users', userRouter);
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
-
-//basic port configuration
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      // the object   the data
+      pelancongan: tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  console.log(req.params);
+  console.log(req.requestTime);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+    // tour is declared above
+  });
+};
+
+exports.createTour = (req, res) => {
+  console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+  // status 200 is ok
+  // status 201 is created
+};
+
+exports.updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour: '<Updated tour here...>' },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  //204 means no content
+  res.status(204).json({
+    status: 'success',
+    data: { tour: '<Updated tour here...>' },
+  });
+};
