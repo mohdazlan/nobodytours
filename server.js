d0e25612a410d3ee7df8af2c995dfb2e35c074bf
azlan <mhdazlanabaziz@gmai.com>
2020-11-27 17:38:48 +0800

    basic database

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB Connection successfull');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: { type: String },
+  rating: Number,
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 console.log(process.env);
 //basic port configuration
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
